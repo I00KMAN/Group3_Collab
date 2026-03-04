fa3c21c6df98d098434624ad35486f3736c32ff4
I00KMAN <PareesLakhman@outlook.com>
2026-03-04 12:50:56 +0000

    I hope its not bricked

---
+++ Source/StalkerGame/StalkerGame.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "StalkerGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, StalkerGame, "StalkerGame" );
