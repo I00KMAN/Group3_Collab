f1c797c7e6e352a550172d74e50892949c46dbac
Kody345 <e020862m@student.staffs.ac.uk>
2026-04-16 14:31:57 +0100

    Converted some stuff to c++
    - Converted most Crouch to c++
    - Changed first person player to derive from MyCharacter class
    - Updated the components that used the blueprint variables to use the c++ ones

--- Source/StalkerGame/C_Crouch.cpp
+++ Source/StalkerGame/C_Crouch.cpp
@@ -4,6 +4,11 @@
 #include "C_Crouch.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
+#include "MyCharacter.h"
+#include "TimerManager.h"
+#include "Curves/RichCurve.h"
+#include "Logging/StructuredLog.h"
+
 
 // Sets default values for this component's properties
 UC_Crouch::UC_Crouch()
@@ -15,19 +20,63 @@ UC_Crouch::UC_Crouch()
 void UC_Crouch::BeginPlay()
 {
 	Super::BeginPlay();
+	
 }
 
 void UC_Crouch::LerpForwardTimer()
 {
+	FTimerManager& TimerManager = GetWorld()->GetTimerManager();
+	FTimerDelegate timerDelegate;
 
+	TimerManager.ClearTimer(FReverseLerpHandle);
+
+	timerDelegate.BindUObject(this, &UC_Crouch::Lerp, GetWorld()->GetDeltaSeconds());
+	TimerManager.SetTimer(FLerpHandle, timerDelegate, GetWorld()->GetDeltaSeconds(), true);
 }
 
 void UC_Crouch::ReverseLerpForwardTimer()
 {
+	FTimerManager& TimerManager = GetWorld()->GetTimerManager();
+	FTimerDelegate timerDelegate;
+
+	TimerManager.ClearTimer(FLerpHandle);
+
+	timerDelegate.BindUObject(this, &UC_Crouch::Lerp, GetWorld()->GetDeltaSeconds() * -1.0f);
+	TimerManager.SetTimer(FReverseLerpHandle, timerDelegate, GetWorld()->GetDeltaSeconds(), true);
 }
 
-void UC_Crouch::AdjustLerpAmount(float Amount)
+void UC_Crouch::Lerp(float Amount)
 {
+	float Max = 0.f;
+	float Min = 0.f;
+
+	FLerpCurve.GetRichCurveConst()->GetTimeRange(Min, Max);
+
+	LerpAmount = FMath::Clamp(LerpAmount + Amount, Min, Max);
+
+	if(Min == LerpAmount)
+	{
+		GetWorld()->GetTimerManager().ClearTimer(FReverseLerpHandle);
+	}
+	else if (Max == LerpAmount) 
+	{
+		GetWorld()->GetTimerManager().ClearTimer(FLerpHandle);
+	}
+
+	OffsetCamera();
+}
+
+void UC_Crouch::OffsetCamera()
+{
+
+	float offsetAmount = 0.f;
+
+	offsetAmount = FMath::Lerp(0.f, CrouchHeightOffset * -1.0f, FLerpCurve.GetRichCurveConst()->Eval(LerpAmount));
+	USpringArm->AddLocalOffset({0.0f, 0.0f, offsetAmount - CurrentOffset });
+
+	UE_LOG(LogTemp, Warning, TEXT("Offset Amount: %f"), offsetAmount - CurrentOffset);
+
+	CurrentOffset = offsetAmount;
 }
 
 void UC_Crouch::TryUnCrouch()
