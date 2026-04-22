365805e2ece4672b863ef91b9197ec094d7010c4
Kody345 <e020862m@student.staffs.ac.uk>
2026-04-22 22:56:30 +0100

    Fixed a bug and started Peek
    - Started peek, only declared functions
    - Fixed a bug in detection UI where if the AI enemy is elevated the UI will skip the middle section

---
+++ Source/StalkerGame/C_Peek.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "C_Peek.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+void UC_Peek::AdjustCameraOffset()
+{
+}
+
+void UC_Peek::LerpForward()
+{
+}
+
+void UC_Peek::LerpReverse()
+{
+}
+
+void UC_Peek::AdjustLerpAmount(float amount)
+{
+}
+
+void UC_Peek::HandlePeek(bool canPeek)
+{
+}
+
+void UC_Peek::SetPlayerPeek_Implementation(bool isPeeking)
+{
+}
+
+
+
---
+++ Source/StalkerGame/C_Peek.h
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "C_Peek.generated.h"
+
+class AMyCharacter;
+class UCameraComponent;
+class USpringArmComponent;
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class STALKERGAME_API UC_Peek : public UActorComponent
+{
+	GENERATED_BODY()
+
+//Functions
+public:	
+	//UC_Peek();
+
+	UFUNCTION()
+	void AdjustCameraOffset();
+
+	//Lerp Logic
+	UFUNCTION()
+	void LerpForward();
+	UFUNCTION()
+	void LerpReverse();
+	UFUNCTION()
+	void AdjustLerpAmount(float amount);
+
+	//Peek Logic
+	UFUNCTION()
+	void HandlePeek(bool canPeek);
+
+protected:
+	//virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintNativeEvent)
+	void SetPlayerPeek(bool isPeeking);
+
+	virtual void SetPlayerPeek_Implementation(bool isPeeking);
+
+private:
+
+//Variables
+public:
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Crouch|Components")
+	TObjectPtr<AMyCharacter> AOwner;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Crouch|Components")
+	TObjectPtr<UCameraComponent> UCamera;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Crouch|Components")
+	TObjectPtr<USpringArmComponent> USpringArm;
+
+	float _lerpAmount;
+	bool _isPeeking;
+private:
+};
